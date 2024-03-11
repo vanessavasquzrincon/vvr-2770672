127645c82c9c3ed161929158e58f45f2401035b8
vanessavasquzrincon <vanessavasquez.2703@gmail.com>
2024-03-11 17:14:05 -0500

    laravel login

---
+++ 04-laravel/larapp/resources/views/auth/login.blade.php
@@ -0,0 +1,68 @@
+{{-- <x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout> --}}
+
+@extends('layouts.app')
+@section('title', 'Login Page - PetsApp')
+@section('content')
+    <header>
+        <img src="{{asset('images/logo.png')}}" alt="Logo">
+    </header>
+    <section class="login">
+        <menu>
+            <a href="javascript:;">Login</a>
+            <a href="{{ url('register/') }}">Register</a>
+        </menu>
+        <form action="{{ route('login') }}" class="form_login" method="post">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+     
+    </section>
+
+@endsection
--- 04-laravel/larapp/routes/web.php
+++ 04-laravel/larapp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,38 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    
 });
 
-Route::get('/sayhello', function () {
-    return "<h1> Hello TCO 2770672 </h1>";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/pets/show', function () {
-    $pets = App\Models\Pet::all();
-    //echo var_dump($pets);
-    dd($pets->toArray());  // Dump & Die
-    
-});
-
-
-Route::get('/pets/view', function () {
-    $pets = App\Models\Pet::all();
-
-    return view('petsview')->with('pets', $pets);
-    
-    
-});
-
-Route::get('/users/show', function () {
-    $users = App\Models\User::all();
-    //echo var_dump($pets);
-    dd($users->toArray());  // Dump & Die
-    
-});
-
-Route::get('/users/view', function () {
-    $users = App\Models\User::take(10)->get();
-
-    return view('usersview')->with('users', $users);
-});
+require __DIR__.'/auth.php';
